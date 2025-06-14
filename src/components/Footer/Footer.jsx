68ef987569e6389ac095fe63f4f2e93e2ea57896
LucDub-code <contact@lucasdubeau.dev>
2025-06-14 12:10:56 +0200

    Corrections sur Footer, gestion de la largeur, cohérence avec le reste de l'app

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,8 +4,10 @@ import logoWhite from "../../assets/logos/kasa-logo-white.svg";
 export default function Footer() {
   return (
     <div className="footer">
+      <div className="footer__container">
       <img className="footer__logo" src={logoWhite} alt="Logo blanc de Kasa" />
       <p className="footer__copyright">© 2020 Kasa. All rights reserved</p>
+      </div>
     </div>
   )
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,7 +6,7 @@ import Gallery from "../Gallery/Gallery";
 export default function Home() {
   return (
     <div>
-      <HeroBanner image={HomeBanner} text="Chez vous, partout et ailleurs" />
+      <HeroBanner image={HomeBanner} text="Chez vous, partout et ailleurs" overlayOpacity={0.6} />
       <Gallery />
     </div>
   )
