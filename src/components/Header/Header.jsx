02e0bd03c724812a31181bf260c5afedcabea912
LucDub-code <lucdubcom@gmail.com>
2025-06-15 16:18:54 +0200

    Ajout du routage et de la navigation active

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import "./Header.scss";
 import logo from "../../assets/logos/kasa-logo.svg";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
@@ -7,8 +8,16 @@ export default function Header() {
       <img className="header__logo" src={logo} alt="Logo de Kasa" />
       <nav className="header__nav">
         <ul className="header__nav-list">
-          <li className="header__nav-item">Accueil</li>
-          <li className="header__nav-item">A propos</li>
+          <li>
+            <NavLink className="header__nav-link" to="/">
+              Accueil
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="header__nav-link" to="/about">
+              A propos
+            </NavLink>
+          </li>
         </ul>
       </nav>
     </div>
