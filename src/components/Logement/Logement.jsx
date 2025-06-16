da5a937768004c0ec21c2a92f93a835630707313
LucDub-code <lucdubcom@gmail.com>
2025-06-16 18:40:24 +0200

    Travail sur les DropdownLogement

--- src/components/Logement/Logement.jsx
+++ src/components/Logement/Logement.jsx
@@ -5,6 +5,7 @@ import { LogementsContext } from "../../context/LogementsContext";
 import NotFound from "../NotFound/NotFound";
 import StarActive from "../../assets/icons/star-active.svg";
 import StarInactive from "../../assets/icons/star-inactive.svg";
+import DropdownLogement from "./DropdownLogement/DropdownLogement";
 
 export default function Logement() {
   const { id } = useParams();
@@ -21,36 +22,48 @@ export default function Logement() {
 
   return (
     <div className="logement">
-      <div className="logement__infos">
-        <h2 className="logement__infos__title">{logement.title}</h2>
-        <p className="logement__infos__location">{logement.location}</p>
-        <div className="logement__infos__tags">
-          {logement.tags.map((tag) => (
-            <span key={tag} className="logement__infos__tags__tag">
-              {tag}
-            </span>
-          ))}
+      <div className="logement__summary">
+        <div className="logement__infos">
+          <h2 className="logement__infos__title">{logement.title}</h2>
+          <p className="logement__infos__location">{logement.location}</p>
+          <div className="logement__infos__tags">
+            {logement.tags.map((tag) => (
+              <span key={tag} className="logement__infos__tags__tag">
+                {tag}
+              </span>
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="logement__host-rating">
-      <div className="logement__host">
-        <p className="logement__host__name">{logement.host.name}</p>
-        <img
-          src={logement.host.picture}
-          alt={logement.host.name}
-          className="logement__host__picture"
-        />
-      </div>
-      <div className="logement__rating">
-        {[...Array(totalStars)].map((_, i) => (
-          <img
-            key={i}
-            src={i < rating ? StarActive : StarInactive}
-            alt={i < rating ? StarActive : StarInactive}
-            className="logement__rating__star"
+        <div className="logement__host-rating">
+          <div className="logement__host">
+            <p className="logement__host__name">{logement.host.name}</p>
+            <img
+              src={logement.host.picture}
+              alt={logement.host.name}
+              className="logement__host__picture"
             />
-          ))}
+          </div>
+          <div className="logement__rating">
+            {[...Array(totalStars)].map((_, i) => (
+              <img
+                key={i}
+                src={i < rating ? StarActive : StarInactive}
+                alt={i < rating ? StarActive : StarInactive}
+                className="logement__rating__star"
+              />
+            ))}
+          </div>
         </div>
+      </div>
+      <div className="logement__dropdowns">
+        <DropdownLogement
+          title="Description"
+          content=""
+        />
+        <DropdownLogement
+          title="Équipements"
+          content=""
+        />
       </div>
     </div>
   );
