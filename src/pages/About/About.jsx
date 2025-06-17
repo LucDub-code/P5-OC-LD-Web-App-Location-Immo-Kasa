01c578ecef489ae4cb8c26fc9a1d5efb93cb42c1
LucDub-code <lucdubcom@gmail.com>
2025-06-17 10:53:44 +0200

    Création d'un dossier pages pour toutes les pages du dossier et création d'un composant réutilisable Dropdown utilisé dans About et Logement

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,26 +1,30 @@
 import "./About.scss";
-import HeroBanner from "../HeroBanner/HeroBanner";
+import HeroBanner from "../../components/HeroBanner/HeroBanner";
 import aboutBanner from "../../assets/images/hero-banner-about.jpg";
-import DropdownAbout from "./DropdownAbout/DropdownAbout";
+import Dropdown from "../../components/Dropdown/Dropdown";
 
 export default function About() {
   return (
     <div className="about">
       <HeroBanner image={aboutBanner} overlayOpacity={0.3} />
       <div className="about__content">
-        <DropdownAbout
+        <Dropdown
+          className="about__dropdown"
           title="Fiabilité"
           content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
         />
-        <DropdownAbout
+        <Dropdown
+          className="about__dropdown"
           title="Respect"
           content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
         />
-        <DropdownAbout
+        <Dropdown
+          className="about__dropdown"
           title="Service"
           content="La qualité du service est au cœur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance."
         />
-        <DropdownAbout
+        <Dropdown
+          className="about__dropdown"
           title="Sécurité"
           content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
         />
