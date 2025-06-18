13d56385aa57b374c61f9d2665b400e456bc360c
LucDub-code <lucdubcom@gmail.com>
2025-06-18 16:03:16 +0200

    Création de la page NotFound

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,16 @@
 import "./NotFound.scss";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className="not-found">
+      <h1 className="not-found__title">404</h1>
+      <p className="not-found__text">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <Link to="/" className="not-found__link">
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
   )
 }
