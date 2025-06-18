8590dbec327df56c7f0d561271012d906629c786
LucDub-code <lucdubcom@gmail.com>
2025-06-18 13:12:12 +0200

    Gestion de la navigation dans le Carrousel avec l'ajout des images prev et next pour une animation de navigation

--- src/pages/Logement/Carrousel,jsx/Carrousel.jsx
+++ src/pages/Logement/Carrousel,jsx/Carrousel.jsx
@@ -1,28 +1,54 @@
 import "./Carrousel.scss";
 import chevronLeft from "../../../assets/icons/chevron-left.svg";
 import chevronRight from "../../../assets/icons/chevron-right.svg";
+import { useState } from "react";
 
 export default function Carrousel({ images }) {
+  const [ImageActive, setImageActive] = useState(0);
+  const previousImage = (ImageActive - 1 + images.length) % images.length;
+  const nextImage = (ImageActive + 1) % images.length;
+
   return (
     <div className="carrousel">
       <img
         className="carrousel__chevron carrousel__chevron--left"
         src={chevronLeft}
         alt="chevron-left"
+        onClick={() =>
+          setImageActive(ImageActive <= 0 ? images.length - 1 : ImageActive - 1)
+        }
       />
       <img
         className="carrousel__chevron carrousel__chevron--right"
         src={chevronRight}
         alt="chevron-right"
+        onClick={() =>
+          setImageActive(ImageActive >= images.length - 1 ? 0 : ImageActive + 1)
+        }
       />
-      {images.map((image, index) => (
-        <img
-          className="carrousel__image"
-          key={index}
-          src={image}
-          alt={`Image ${index + 1}`}
-        />
-      ))}
+      {images.map((image, index) => {
+        if (
+          index === ImageActive ||
+          index === previousImage ||
+          index === nextImage
+        ) {
+          return (
+            <img
+              key={index}
+              className={`carrousel__image carrousel__image--${
+                index === ImageActive
+                  ? "active"
+                  : index === previousImage
+                  ? "prev"
+                  : "next"
+              }`}
+              src={image}
+              alt={`Image ${index + 1}`}
+            />
+          );
+        }
+        return null;
+      })}
     </div>
   );
 }
