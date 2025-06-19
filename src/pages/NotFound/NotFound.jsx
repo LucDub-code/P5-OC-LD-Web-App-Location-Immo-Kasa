4fd833453f99440aaec724f09a51d07911d38ad2
LucDub-code <lucdubcom@gmail.com>
2025-06-19 17:19:26 +0200

    Responsive de la page NotFound

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -6,7 +6,7 @@ export default function NotFound() {
     <div className="not-found">
       <h1 className="not-found__title">404</h1>
       <p className="not-found__text">
-        Oups! La page que vous demandez n'existe pas.
+        {<>Oups! La page que <br className="not-found__break" /> vous demandez n'existe pas.</>}
       </p>
       <Link to="/" className="not-found__link">
         Retourner sur la page d'accueil
